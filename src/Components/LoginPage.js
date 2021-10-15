ec5d7b1f7a6508b67aecbdc9ffdc9854035f1c5c
Saksham <2020UCP1801@mnit.ac.in>
2021-10-15 10:40:06 +0530

    login component

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function LoginPage() {
-  return <div id="loginPage" className="authComponent"></div>;
+  return (
+    <div id="loginPage" className="authComponent">
+      <h2>Login</h2>
+      <form action="" className="authForm">
+        <input type="number" placeholder="Enter mobile number" />
+      </form>
+    </div>
+  );
 }
