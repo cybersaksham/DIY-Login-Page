3384ec794e49730761f4b508a6228200f63c0540
Saksham <2020UCP1801@mnit.ac.in>
2021-10-16 11:26:37 +0530

    otp input logic

--- frontend/src/Components/LoginPage.js
+++ frontend/src/Components/LoginPage.js
@@ -1,11 +1,84 @@
 import React from "react";
 
 export default function LoginPage() {
+  const keyCodes = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "Backspace",
+  ];
+
+  const checkOTPInput = () => {
+    const otpInputs = document.getElementsByClassName("otpInputField");
+    for (let i = 0; i < Array.from(otpInputs).length; i++) {
+      const val = otpInputs[i].value;
+      if (val === "") return i;
+    }
+    return Array.from(otpInputs).length - 1;
+  };
+
+  const changeFocus = () => {
+    const otpInputs = document.getElementsByClassName("otpInputField");
+    otpInputs[checkOTPInput()].focus();
+  };
+
+  const keyDown = (e) => {
+    e.preventDefault();
+    const key = e.key;
+    if (!keyCodes.includes(key)) return;
+    const otpInputs = document.getElementsByClassName("otpInputField");
+    if (key !== "Backspace") {
+      if (e.target.value === "") e.target.value = Number(e.key);
+    } else {
+      if (e.target.value !== "") e.target.value = "";
+      else {
+        let i = checkOTPInput();
+        if (i > 0) otpInputs[i - 1].value = "";
+      }
+    }
+    changeFocus();
+  };
+
   return (
     <div id="loginPage" className="authComponent">
       <h2>Login</h2>
       <form action="" className="authForm">
+        <label htmlFor="">Mobile</label>
         <input type="number" placeholder="Enter mobile number" />
+        <label htmlFor="">OTP</label>
+        <div id="otpInput">
+          <input
+            className="otpInputField"
+            onFocus={() => changeFocus()}
+            onKeyDown={(e) => keyDown(e)}
+            type="number"
+          />
+          <input
+            className="otpInputField"
+            onFocus={() => changeFocus()}
+            onKeyDown={(e) => keyDown(e)}
+            type="number"
+          />
+          <input
+            className="otpInputField"
+            onFocus={() => changeFocus()}
+            onKeyDown={(e) => keyDown(e)}
+            type="number"
+          />
+          <input
+            className="otpInputField"
+            onFocus={() => changeFocus()}
+            onKeyDown={(e) => keyDown(e)}
+            type="number"
+          />
+        </div>
       </form>
     </div>
   );
