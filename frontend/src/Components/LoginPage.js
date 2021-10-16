98c4823b2c5ec6309b6b9e0802db7a348f3fe773
Saksham <2020UCP1801@mnit.ac.in>
2021-10-16 12:12:09 +0530

    google login button

--- frontend/src/Components/LoginPage.js
+++ frontend/src/Components/LoginPage.js
@@ -54,6 +54,14 @@ export default function LoginPage() {
     });
   };
 
+  function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+
   return (
     <div id="loginPage" onSubmit={sendOtp} className="authComponent">
       <h2>Login</h2>
@@ -95,6 +103,7 @@ export default function LoginPage() {
           Send OTP
         </button>
       </form>
+      <div className="g-signin2" data-onsuccess="onSignIn"></div>
     </div>
   );
 }
