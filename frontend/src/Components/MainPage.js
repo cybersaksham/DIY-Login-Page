dcc4cc5329fecf0ef3955766c6f7ddcd7923aceb
Saksham <2020UCP1801@mnit.ac.in>
2021-10-18 13:14:36 +0530

    direct login bug fixed

--- frontend/src/Components/MainPage.js
+++ frontend/src/Components/MainPage.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 export default function MainPage(props) {
   const { name } = props;
+  const history = useHistory();
+
+  if (name === "") history.push("/");
 
   return <div>Logged in as {name}</div>;
 }
